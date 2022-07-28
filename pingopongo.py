b7db2cde10654e26d9f65902286e356aa0e1371d
Farid1405 <52379088+Farid1405@users.noreply.github.com>
2022-07-28 10:20:57 +0500

    Созданы 2 игрока
    Но при этом остаются следы

--- pingopongo.py
+++ pingopongo.py
@@ -1,6 +1,5 @@
 from pygame import *
 
-
 class GameSprite(sprite.Sprite):
     def __init__(self,player_image, player_x,player_y,
     player_width,player_height ,player_speed):
@@ -19,8 +18,36 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < 400:
+            self.rect.y += self.speed
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-    
+        if keys[K_DOWN] and self.rect.y < 400:
+            self.rect.y += self.speed
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width,win_height))
+window.fill((200,255,255))
+clock = time.Clock()
+display.set_caption("PingPong")
+FPS = 60
+game = True
+finish = False
+
+racket1 = Player('racket.png', 5, win_height / 2, 30, 100, 10)
+racket2 = Player('racket.png', win_width-30, win_height / 2, 30, 100, 10)
+
+while game:
+    if not finish:
+        racket1.reset()
+        racket2.reset()
+        racket1.update_l()
+        racket2.update_r()
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
+    clock.tick(FPS)
+
